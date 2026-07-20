4fd5b7b0787c65541e9ac7ee1885267fa57d2d79
todoom1 <aa13976383@outlook.com>
2026-07-20 13:52:47 +0800

    Add files via upload

--- inpro.js
+++ inpro.js
@@ -1,5 +1,4 @@
 const apps = [
-{id: 'tyzb', img: './png/tyzb.jpg', name: '赛事直播', url: 'https://damizhibo.com/', cat: 'gemes'},
 {id: 'pg', img: './svg/pg.svg', name: 'PG模拟器', url: './tea1.html', cat: 'gemes'},
 {id: 'pp', img: './svg/pp.svg', name: 'PP模拟器', url: './pptest.html', cat: 'gemes'},
 {id: 'hackmnq', img: './svg/pg.svg', name: 'HS模拟器', url: './hacksaw.html', cat: 'gemes'},
@@ -17,14 +16,13 @@ const apps = [
 {id: 'bjl', img: './png/qt.png', name: '百家乐模拟器', url: 'https://www.ab8888.games:8888/?loginType=2&amp;trial=true&amp;language=zh_CN', cat: 'gemes'},
 {id: 'cpmn', img: './png/cp.png', name: '彩票模拟器', url: 'https://pc.obcp.live/redirect.html', cat: 'gemes'},
 {id: 'oy', img: './svg/oy.svg', name: '欧易', url: 'https://www.vmutkhamuut.com/join/40221549', cat: 'bths'},
-{id: 'ba', img: './svg/ba.svg', name: '币安', url: 'https://www.maxweb.academy/referral/earn-together/refer-in-hotsummer/claim?hl=zh-CN&ref=GRO_20338_SI463&utm_source=default', cat: 'bths'},
-{id: 'xnb', img: './png/npc.png', name: '虚拟币入门', url: './xnb.html', cat: 'bths'},
-{id: 'ggy', img: './png/ggy.png', name: '脉动vpn(狗云)', url: 'https://maidongyun.com/#/register?code=V5fllz2y', cat: 'tools'},
+{id: 'ba', img: './svg/ba.svg', name: '币安', url: 'https://www.bsmkweb.cc/register?ref=H67DQWYZ', cat: 'bths'},
+{id: 'xnb', img: './svg/btc.svg', name: '虚拟币入门', url: './xnb.html', cat: 'bths'},
+{id: 'ggy', img: './png/ggy.png', name: '脉动vpn(狗云)', url: 'https://maidongvpn.com/#/register?code=V5fllz2y', cat: 'tools'},
 {id: 'dkd', img: './png/dkd.png', name: '杜卡迪vpn', url: 'https://www.dukadi.xyz/#/register?code=ZrNq0rDZ', cat: 'tools'},
 {id: 'dm', img: './png/vpn.png', name: '大麦VPN', url: 'https://666.damai222.cc/#/register?code=JGQT6iHz', cat: 'tools'},
 {id: 'jryw', img: './png/nullb.png', name: '今日要闻', url: 'https://60s.lylme.com/', cat: 'tools'},
 {id: 'hwid', img: './svg/id.svg', name: '海外ID', url: 'https://id.ali-door.top/share/damaiye', cat: 'tools'},
-{id: 'rjdq', img: './png/rjdq.png', name: '软件大全', url: 'https://52qfx.lanzouw.com/b01jdki3a', cat: 'tools'},
 {id: 'dxjm', img: './svg/dxjm.svg', name: '短信接码', url: 'http://www.eomsg.com/appweb/signUp.html?inviter=1o0je3kd', cat: 'tools'},
 {id: 'htyw', img: './png/nullb.png', name: '花体英文', url: 'https://1000tool.com/text/penmanship/', cat: 'tools'},
 {id: 'jable', img: './svg/jable.svg', name: 'jable', url: 'https://jable.tv/', cat: 'tools'},
@@ -50,10 +48,7 @@ function renderApps() {
         li.id = app.id;
 
         li.innerHTML = `
-            <div class="app-icon">
-                <img src="${app.img}" alt="${app.name}">
-            </div>
-            <div class="app-name">${app.name}</div>
+            <div class="app-icon"><img src="${app.img}" alt="${app.name}"></div><div class="app-name">${app.name}</div>
         `;
         li.addEventListener('click', () => {
             window.open(app.url, '_blank', 'noopener,noreferrer');
@@ -63,7 +58,7 @@ function renderApps() {
     });
 }
 renderApps();
-document.getElementById("52").innerHTML="🔥PG模拟器大全";
+document.getElementById("52").innerHTML="PG模拟器";
 document.getElementById("54").innerHTML="虚拟币";
-document.getElementById("55").innerHTML="其他分享&VPN-打不开的网站请开VPN";
+document.getElementById("55").innerHTML="实用工具";
 document.getElementById("56").innerHTML="如遇本站功能无法正常使用，请点击下载VPN，即可突破网络封锁正常使用";
